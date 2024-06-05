5830476ce79eecf37a08e1382be4ed5af0b4b4c0
Yasen Tsvetkov <yasentsv@gmail.com>
2024-06-05 22:33:23 +0300

    az i curo naprawihme menuto

--- src/menu.c
+++ src/menu.c
@@ -6,29 +6,20 @@
 int main()
 {
     setlocale(LC_ALL, "");
-    printf("***************************\n");
-    printf("*     STANI BOGAT         *\n");
-    printf("***************************\n");
-
+    
     unsigned char c;
     int op = 1;
     do
     {
         system("cls");
 
-        if (op == 1)
-            printf(">1. Zapochni igra\n");
-        else
-            printf(" 1. Zapochni igra\n");
-        if (op == 2)
-            printf(">2. Pravila na igrata\n");
-        else
-            printf(" 2. Pravila na igrata\n");
+        printf("***************************\n");
+        printf("*     STANI BOGAT         *\n");
+        printf("***************************\n");
 
-        if (op == 3)
-            printf(">3. Izhod\n");
-        else
-            printf(" 3. Izhod\n");
+        if (op == 1)printf(">1. Zapochni igra\n");else printf(" 1. Zapochni igra\n");
+        if (op == 2)printf(">2. Pravila na igrata\n");else printf(" 2. Pravila na igrata\n");
+        if (op == 3)printf(">3. Izhod\n");else printf(" 3. Izhod\n");
 
         c = getch();
 
@@ -38,16 +29,27 @@ int main()
             break;
         }
 
-        if (c == 13)
+        if (c == 13) // Enter
         {
-            if(op == 2) {
+            if(op == 1){
+                printf("Zapochna igra\n");
+                break; // Ralchev da pishe kod
+            }else if(op == 2) {
+                printf("----------------------------------------------------------------------------------------------\n");
                 printf("1. Imate 10 vyprosa s narastvashta trudnost.\n");
                 printf("2. Vseki vypros ima 4 vyzmozhni otgovora, no samo edin e praven.\n");
                 printf("3. Mozhete da izpolzvate tri jokera: 50:50, Pomosht ot publikata i Obadi se na priyatel.\n");
-                printf("4. Za da spechelite golyamata nagrada, tryabva da otgovorite praveno na vsichki vyprosi.\n\n");
+                printf("4. Za da spechelite golyamata nagrada, tryabva da otgovorite praveno na vsichki vyprosi.\n");
+                printf("----------------------------------------------------------------------------------------------\n");
+
+                printf("Natisni kojto i da e klavish za da produljish...\n\n");
+
+                getch(); 
+
+            }else if(op == 3) {
+                printf("Izhod\n");
+                break;
             }
-            printf("\nYou have chosen option: %d", op);
-            break;
         }
 
         if (c == 224 || c == 0)
@@ -57,12 +59,12 @@ int main()
             {
                 op--;
                 if (op < 1)
-                    op = 6;
+                    op = 3;
             }
             else if (c == 80)
             {
                 op++;
-                if (op > 6)
+                if (op > 3)
                     op = 1;
             }
         }
